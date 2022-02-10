063b09ba3650c943585dc1ba87f19f05a4891ba7
Yanis Amrouche <54064644+yanisamrouche@users.noreply.github.com>
2022-02-10 20:58:32 +0100

    Add files via upload
    update creation album avec la bdd

--- projet/creationalbumwindow.cpp
+++ projet/creationalbumwindow.cpp
@@ -1,6 +1,8 @@
 #include "creationalbumwindow.h"
 #include "database.h"
 #include <QDebug>
+#include <iostream>
+using namespace std;
 
 CreationAlbumWindow::CreationAlbumWindow(QWidget *parent) :
     QDialog(parent), ui_creationAlbum(new Ui::CreationAlbumWindow)
@@ -18,14 +20,28 @@ CreationAlbumWindow::CreationAlbumWindow(QWidget *parent) :
 void CreationAlbumWindow::on_create_album_btn_clicked()
 {
    name = ui_creationAlbum->album_name->toPlainText();
+   int id_album = createAlbum(name.toStdString());
    int nbrOfItems = ui_creationAlbum->images_list->count();
    qDebug() << name;
    qDebug() << nbrOfItems;
    for(int i=0; i<nbrOfItems; i++){
         img_paths.append(ui_creationAlbum->images_list->item(i)->text());
+        createImage(id_album,"",img_paths.at(i).toStdString(),"",0,0,0,0,0);
    }
    qDebug() << img_paths;
-   //createAlbum(name.toStdString());
+   qDebug() << "======================================================";
+   qDebug() << "les albums : ";
+   qDebug() << getAlbums();
+   qDebug() << "le titre de l'album crée : ";
+   cout << getTitleAlbum(id_album);
+   qDebug() << "les images de l'album crée : ";
+   qDebug() << getAllImages(id_album);
+   std::vector<int> images = getAllImages(id_album);
+   for(int i : images){
+       qDebug() << i;
+       qDebug() << getPathImage(i).c_str();
+   }
+   qDebug() << "======================================================";
 
 }
 
